504d4243c6a1d8739589f22f16fdbd8f0e7b811c
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2020-11-11 18:07:19 +0700

    Fixed Chaining Middleware

--- chaining-middleware/main.go
+++ chaining-middleware/main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"net/http"
-	// "fmt"
-	// "encoding/json"
+	"fmt"
+	"encoding/json"
 )
 
 type city struct {
-	Name string
-	Area uint64
+	Name string `json:name`
+	Area uint64	`json:area`
 }
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
 	// check if method is POST
 	if r.Method == "POST" {
+		var tempCity city
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&tempCity)
+		if err != nil {
+			panic(err)
+		}
+		defer r.Body.Close()
+		// Your resource creation logic goes here. For now it is plain print to console
+		fmt.Printf("Got %s city with area of %d sq miles!\n", tempCity.Name, tempCity.Area)
 		// tell everything is fine
 		w.WriteHeader(http.StatusCreated)
 		w.Write([]byte("201 - Created"))
