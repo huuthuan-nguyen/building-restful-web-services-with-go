80052f2e7be7dfe35ad0d384c508a2b8bf28fb75
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2020-11-11 14:37:12 +0700

    grouping router with subrouter

--- gorilla-mux/main.go
+++ gorilla-mux/main.go
@@ -18,12 +18,45 @@ func ArticleHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "ID is: %v\n", vars["id"])
 }
 
+func AuthorArticle(w http.ResponseWriter, r *http.Request) {
+	// mux.Vars returns all path parameters as a map
+	vars := mux.Vars(r) // parameters map
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Author is: %v\n", vars["name"])
+	fmt.Fprintf(w, "ID is: %v\n", vars["id"])
+}
+
+// settings handler
+func settingsHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // parameters map
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Setting ID: %v\n", vars["id"])
+}
+
+func detailsHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // parameters map
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Detail ID: %v\n", vars["id"])
+}
+
 func main() {
 	// create a new router
 	r := mux.NewRouter()
 
 	// attach a path with handler
 	r.HandleFunc("/articles/{category}/{id:[0-9]+}", ArticleHandler)
+
+	// define preceding route
+	r.Path("/authors/{name}/{id:[0-9]+}").HandlerFunc(AuthorArticle)
+
+	// subrouter, useful for grouping router
+	s := r.PathPrefix("/config").Subrouter()
+	s.HandleFunc("/{id:[0-9]+}/settings", settingsHandler)
+	s.HandleFunc("/{id:[0-9]+}/details", detailsHandler)
+
 	server := &http.Server{
 		Handler: r,
 		Addr: ":8080",
